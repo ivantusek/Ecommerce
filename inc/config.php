be847e03a3c331a40ef470d1c7107bdfa6d475bf
rippercro <itusek.os@gmail.com>
2017-10-19 15:14:29 +0200

    Defined folders path in config.php

--- inc/config.php
+++ inc/config.php
@@ -11,3 +11,31 @@ defined("SITE_URL")
 // Directory separator
 defined("DS")
     || define("DS", DIRECTORY_SEPARATOR);
+
+// Root path
+defined("ROOT_PATH")
+    || define("ROOT_PATH", realpath(dirname(__FILE__) .DS."..".DS));
+
+// Classes folder
+defined("CLASSES_DIR")
+    || define("CLASSES_DIR", "classes");
+
+// Modules folder
+defined("MOD_DIR")
+    || define("MOD_DIR", "mod");
+
+// Inc folder
+defined("INC_DIR")
+    || define("INC_DIR", "inc");
+
+// Template folder
+defined("TEMPLATE_DIR")
+    || define("TEMPLATE_DIR", "template");
+
+// Emails path
+defined("EMAILS_PATH")
+    || define("EMAILS_PATH", ROOTH_PATH.DS."emails");
+
+// Catalogue images path
+defined("CATALOGUE_PATH")
+    || define("CATALOGUE_PATH", ROOTH_PATH.DS."media".DS."catalogue");
