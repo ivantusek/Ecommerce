0a03feca9d4fa57d6e0d276eea5f1bba20258f52
rippercro <itusek.os@gmail.com>
2017-10-19 14:34:34 +0200

    Create template folder and create header and footer php files

---
+++ template/footer.php
@@ -0,0 +1,11 @@
+</div>
+<div class="cl">&#160;</div>
+</div>
+</div>
+<div id="footer">
+    <div id="footer_in">
+        <a href="#" title="Footer" target="_blank">Footer</a>
+    </div>
+</div>
+</body>
+</html>
