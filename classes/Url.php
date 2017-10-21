a09326a41caeadf13d583233e0d9d2de8b5e9382
rippercro <itusek.os@gmail.com>
2017-10-21 15:15:03 +0200

    Url class.

---
+++ classes/Url.php
@@ -0,0 +1,14 @@
+<?php
+// Url class
+class Url {
+
+    public static $_page = "page";
+    public static $_folder = PAGES_DIR;
+    public static $_params = array ();
+
+    public static function getParam(){
+
+
+    }
+
+}
