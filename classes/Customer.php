cd2ad9dc87826fd477bd9ef6619c8fe6209ad1ae
rippercro <itusek.os@gmail.com>
2018-02-24 13:54:52 +0100

    Edit customer profile

--- classes/Customer.php
+++ classes/Customer.php
@@ -92,5 +92,42 @@ class Customer{
         return $result;
     }
 
+    public function customerUpdate($data, $cmrId)
+    {
+        $name = mysqli_real_escape_string($this->db->link, $data['name']);
+        $address = mysqli_real_escape_string($this->db->link, $data['address']);
+        $city = mysqli_real_escape_string($this->db->link, $data['city']);
+        $country = mysqli_real_escape_string($this->db->link, $data['country']);
+        $zip = mysqli_real_escape_string($this->db->link, $data['zip']);
+        $phone = mysqli_real_escape_string($this->db->link, $data['phone']);
+        $email = mysqli_real_escape_string($this->db->link, $data['email']);
+
+        if ($name == "" || $address == "" || $city == "" || $country == "" || $zip == "" || $phone == "" || $email == "" || $email == "") {
+            $msg = "<span class='error'>Fields must not be empty!</span>";
+            return $msg;
+        } else {
+
+            $query = "UPDATE customer
+                  SET 
+                    name     = '$name',
+                    address  = '$address',
+                    city     = '$city',
+                    country  = '$country',
+                    zip      = '$zip',
+                    phone    = '$phone',
+                    email    = '$email'
+                  WHERE id   = '$cmrId'";
+
+            $updated_row = $this->db->update($query);
+            if ($updated_row) {
+                $msg = "<span class='success'>Customer data updated successfully!</span>";
+                return $msg;
+            } else {
+                $msg = "<span class='error'>Customer data not updated!</span>";
+                return $msg;
+            }
+        }
+    }
+
 
 }
--- editprofile.php
+++ editprofile.php
@@ -6,6 +6,12 @@
     }
 
 ?>
+<?php
+    $cmrId = Session::get("cmrId");
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])){
+        $updateCmr = $cmr->customerUpdate($_POST, $cmrId);
+    }
+?>
     <style>
         .tblone
         {
@@ -17,62 +23,62 @@
         {
             text-align:justify;
         }
+
+        .tblone input[type="text"]
+        {
+            width:400px;
+            padding:5px;
+            font-size:15px;
+        }
     </style>
     <div class="main">
         <div class="content">
             <div class="section group">
-
                 <?php
-                    $id = Session::get("cmrId");
-                    $getdata = $cmr->getCustomerData($id);
-                    if ($getdata){
-                        while($result = $getdata->fetch_assoc()){
+                $id = Session::get("cmrId");
+                $getdata = $cmr->getCustomerData($id);
+                if ($getdata){
+                    while($result = $getdata->fetch_assoc()){
                         ?>
                         <form action="" method="POST">
                             <table class="tblone">
+                                <?php if (isset($updateCmr)){
+                                    echo " <tr><td></td><td colspan='2'>".$updateCmr."</td></tr>";}?>
                                 <tr>
                                     <td></td>
-                                    <td colspan="3" font-color="red">Your Profile Details</td>
+                                    <td colspan="2" font-color="red">Update Profile Details</td>
                                 </tr>
                                 <tr>
                                     <td width="20%">Name</td>
-                                    <td width="5%">:</td>
-                                    <td><?php echo $result['name'];?></td>
+                                    <td><input type="text" name="name" value="<?php echo $result['name'];?>"></td>
                                 </tr>
                                 <tr>
                                     <td width="20%">Phone</td>
-                                    <td width="5%">:</td>
-                                    <td><?php echo $result['phone'];?></td>
+                                    <td><input type="text" name="phone" value="<?php echo $result['phone'];?>"></td>
                                 </tr>
                                 <tr>
                                     <td>Email</td>
-                                    <td>:</td>
-                                    <td><?php echo $result['email'];?></td>
+                                    <td><input type="text" name="email" value="<?php echo $result['email'];?>"></td>
                                 </tr>
                                 <tr>
                                     <td>Address</td>
-                                    <td>:</td>
-                                    <td><?php echo $result['address'];?></td>
+                                    <td><input type="text" name="address" value="<?php echo $result['address'];?>"></td>
                                 </tr>
                                 <tr>
                                     <td>City</td>
-                                    <td>:</td>
-                                    <td><?php echo $result['city'];?></td>
+                                    <td><input type="text" name="city" value="<?php echo $result['city'];?>"></td>
                                 </tr>
                                 <tr>
                                     <td>Zipcode</td>
-                                    <td>:</td>
-                                    <td><?php echo $result['zip'];?></td>
+                                    <td><input type="text" name="zip" value="<?php echo $result['zip'];?>"></td>
                                 </tr>
                                 <tr>
                                     <td>Country</td>
-                                    <td>:</td>
-                                    <td><?php echo $result['country'];?></td>
+                                    <td><input type="text" name="country" value="<?php echo $result['country'];?>"></td>
                                 </tr>
                                 <tr>
                                     <td></td>
-                                    <td></td>
-                                    <td><a href="editprofile.php">Update details</a></td>
+                                    <td><input type="submit" name="submit" value="Save"></td>
                                 </tr>
                             </table>
                         </form>
